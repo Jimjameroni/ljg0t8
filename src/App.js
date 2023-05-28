a551330ea865b317b0ca55e76e329c548b8654f9
Jimjameroni <Jimbits@hotmail.co.uk>
2023-05-28 14:37:05 +0100

    Remove bad spacing

--- src/App.js
+++ src/App.js
@@ -82,9 +82,9 @@ function calculateWinner(squares){
   ]
   for (let i = 0;i < lines.length; i++) {
     const [a, b, c] = lines[i];
-   if (squares[a] === squares[b] && squares[a] === squares[c]){
+  if (squares[a] === squares[b] && squares[a] === squares[c]){
     return squares[a];
-   } 
+  }
   }
   return null;
   
